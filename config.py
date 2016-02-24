fb3d07ba417d3f79353e23b12b4c9067969e1241
Unplugged <se4@live.cn>
2016-02-24 21:12:02 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#小助配置部分
+mid= 4766 #萌小助后台>接口信息>调试公众号>>地址栏service_id后头的四位数字
+mode = 0 #0 为模板信息接口,#1 为48h客服接口的文本信息接口
+
+#公众号配置部分
+appid = ''
+appsecret = ''
+
+
+#(手工群发用)模板信息配置
+templateid = 'FRtdSZlahbwk8Oo2Fg-7mw7AgLhZ1mILoNbhtOCILkk' #微信官方后台申请 考生姓名/学校/准考证号/成绩详情 版本的模板
+templateurl = ''
+templatefirst = ''
+templatekeyword1 = ''
+templatekeyword2 = ''
+templatekeyword3 = ''
+templatekeyword4 = ''
+templateremark = ''
